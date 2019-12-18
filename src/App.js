9388b28255839fee238f86d59c8722d6f7912d07
Evan Schwarz <evanschwarz90@gmail.com>
2019-12-18 11:43:47 -0700

    test push from my machine

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import './App.css';
 const App = () => {
 
   const [numTeams, setNumTeams] = useState(0); 
+  const [teamNames, setTeamNames] = useState([]);
 
   const handleNumTeams = () => {
     setNumTeams(numTeams + 1)
+  }
+
+  const addTeam = (name) => {
+    teamNames.push(name);
+    setTeamNames(teamNames);
   }
 
   return (
