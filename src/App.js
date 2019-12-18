22636896b09bccc55b99c56ce0b7d4261aba885a
Evan Schwarz <evanschwarz90@gmail.com>
2019-12-18 11:45:06 -0700

    test push from branch

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
 
   const [numTeams, setNumTeams] = useState(0); 
   const [teamNames, setTeamNames] = useState([]);
+  const [page, setPage] = useState(0);
 
   const handleNumTeams = () => {
     setNumTeams(numTeams + 1)
