7c2f610d6e8c7eedb431143c00aa8fa878103332
Axel Brostedt <axelbrostedt@gmail.com>
2020-01-16 11:18:03 -0700

    Adds position and animation to second team

--- src/Match.js
+++ src/Match.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './App.css';
 import Team from './Team.js';
+import Team2 from './Team2.js';
 
 const Match = (props) => {
 
@@ -8,9 +9,16 @@ const Match = (props) => {
         <div>
             <div className="match-container">
                 {/* <div className="horizontal"></div> */}
-                <Team />
-                <div className="round1">
+                <Team className="team"/>
+                <div className="round1" onClick={e => {
+                    let top = e.target.offsetTop + e.target.offsetHeight;
+                    console.log(top);
+                    const $team2 = document.getElementsByClassName('team2');
+                    // console.log($team2);
+                    $team2[0].style.marginTop = `80%`;
+                }}>
                 </div>
+                <Team2 className="team2"/>
             </div>
         </div>
     )
---
+++ src/Team2.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+import './App.css';
+
+
+const Team2 = (props) => {
+
+
+    return (
+        <div>
+            <div className="team2"></div>
+        </div>
+    )
+}
+
+export default Team2;
