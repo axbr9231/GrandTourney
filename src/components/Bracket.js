27e0403c7f755c5a962c8fe9f9af2a4b105a03e3
Evan Schwarz <evanschwarz90@gmail.com>
2020-02-26 16:08:49 -0700

    adds a rounds component

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -4,23 +4,32 @@ import Match from './Match.js'
 import Grid from '@material-ui/core/Grid';
 
 const Bracket = ({ teamArray }) => {
-    const matches = [];
-
+    const originalRoundObject = {};
+    
+    const firstRoundMatches = [];
     let match = [];
     while (teamArray.length) {
         match.push(teamArray.splice(Math.floor(Math.random() * teamArray.length), 1)[0]);
         match.push(teamArray.splice(Math.floor(Math.random() * teamArray.length), 1)[0]);
-        matches.push(match);
+        firstRoundMatches.push(match);
         match = [];
     }
+    originalRoundObject[1] = firstRoundMatches;
 
-    console.log('matches: ', matches);
+    const numRounds = Math.ceil(Math.log2(teamArray.length));
+    for (let i = 2; i <= numRounds; i++) {
+        originalRoundObject[i] = []
+    };
+
+    const [rounds, updateRounds] = useState(originalRoundObject);
+
+    console.log('rounds: ', rounds);
 
     return (
         <div id="bracket">
             <Grid container >
                 <Grid item>
-                {matches.map((match, i) => {
+                {rounds[1].map((match, i) => {
                     console.log('hit at match number ', i);
                     return (
                         <Match round="round1" key={i} teams={match} />
---
+++ src/components/Round.js
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import '../App.css';
+import Match from './Match.js'
+
+const Round = (props) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Round;
