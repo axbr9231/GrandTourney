10b7e172d4e74e91a6d9b7c2015c14b23b92ecdb
Evan Schwarz <evanschwarz90@gmail.com>
2020-03-28 15:34:02 -0600

    finishes refactoring to styled components

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -1,17 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
 import '../App.css';
 import Round from './Round.js';
 
-const Bracket = ({ rounds, updateNextRound, currentMatch }) => {
+const Container = styled.div`
+    width: 80vw;
+    min-height: 100vh;
+    background-color: chartreuse;
+    padding-left: 25px;
+    display: flex;
+`;
 
-    
-    
-    
+const Bracket = ({ rounds, updateNextRound, currentMatch }) => {    
 
     return (
-        <div id="bracket">
+        <Container>
             {Object.keys(rounds).map(round => <Round key={round} round={round} matches={rounds[round]} updateNextRound={updateNextRound} currentMatch={currentMatch} />)}
-        </div>
+        </Container>
     )
 }
 
